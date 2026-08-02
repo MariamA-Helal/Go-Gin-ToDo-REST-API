1ab7d8c1c0c0b43ffbd88288f661940b115800e4
MariamA_Helal <mariamamr28624@gmail.com>
2026-08-02 17:09:29 +0300

    feat: test raw sql connection and basic queries with PostgreSQL

---
+++ scripts/raw-sql-test.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	// Database connection parameters
+	connStr := "host=localhost user=postgres password=0000 dbname=todo_app sslmode=disable"
+
+	// Open a connection to the database
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Insert
+	_, err = db.Exec("INSERT INTO todos (title) VALUES ($1)", "Learn PostgreSQL")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Query
+	rows, err := db.Query("SELECT id, title, completed FROM todos")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id int
+		var title string
+		var completed bool
+		if err := rows.Scan(&id, &title, &completed); err != nil {
+			log.Fatal(err)
+		}
+		log.Println(id, title, completed)
+	}
+}
